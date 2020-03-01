9a7153798e1c8d904308aee2752df616a93a4ae8
Pixel1011 <enderdragon1105@gmail.com>
2020-03-01 18:26:23 +0000

    more write

--- OpenComputers/reactor.lua
+++ OpenComputers/reactor.lua
@@ -3,6 +3,8 @@ local term = require("term")
 local red = component.redstone
 local reactor = component.nc_fission_reactor
 local c = component.computer
+local gpu = component.gpu
+local active = reactor.isProcessing()
 --local list = component.list()
 --for address, componentType in list do
 --  print("Address: ", address," | component: ", componentType)
@@ -10,12 +12,35 @@ local c = component.computer
 
 function write() 
   term.clear()
-  term.setCursor(0,0)
-  term.write(" Nuclearcraft".. reactor.getLengthX() .. reactor.getLengthY() .. reactor.getLengthZ() .. "fission reactor controler")
+  term.setCursor(1,0)
+  term.write("Nuclearcraft ".. reactor.getLengthX() .. "x" .. reactor.getLengthY() .. "x" .. reactor.getLengthZ() .. "fission reactor controler")
+  term.setCursor(1,1)
+  if (active == true) 
+    local onoff = "Online"
+    gpu.setForeground(0x37ff00)
+  end
+  if (active == false)
+    local onoff = "Offline"
+    gpu.setForeground(0xff0000)
+  end
+  term.write("Reactor: ", onoff)
+  gpu.setForeground(0xffffff)
   os.sleep(1)
+end
+
+function start()
+  if(active == false) 
+    --reactor.activate()
+    --active = true;
+    --monitor()
+  end
+end
+
+function monitor()
+  
 end
 
 while true do 
   write()
-  c.beep()
+  --c.beep()
 end
